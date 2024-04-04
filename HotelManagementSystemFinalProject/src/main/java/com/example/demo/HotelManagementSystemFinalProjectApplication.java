040931b62636991a9b3410298cab3f925c17821d
KeiranMacKay <keiran.mackay@ontariotechu.net>
2024-04-03 20:01:18 -0400

    springboot starter project

---
+++ HotelManagementSystemFinalProject/src/main/java/com/example/demo/HotelManagementSystemFinalProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelManagementSystemFinalProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelManagementSystemFinalProjectApplication.class, args);
+	}
+
+}
